99338f2b385de7dad0cf6699a1f7e4de75087caa
Gregory Hellings <greg.hellings@gmail.com>
2017-03-06 14:29:13 +0000

    Only install missing/outdated modules

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 function mods {
 	yes yes | installmgr -r "$1"
-	MODS=$(installmgr -rl "$1" | sed -E -e 's/.*\[(.*?)\].*/\1/' | sed -e '1,3d')
+	MODS=$(installmgr -rd "$1" | sed -E -e 's/.*\[(.*?)\].*/\1/' | sed -e '1,3d')
 	for mod in $MODS; do
 		yes yes | installmgr -ri "$1" $mod;
 	done
